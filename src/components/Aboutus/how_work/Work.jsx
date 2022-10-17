6f137906516b3480df96fbf5ed24925bb2ffc7f5
kushalpandey <kushal.pandey118@gmail.com>
2022-10-17 09:08:40 +0545

    kushal commit

---
+++ src/components/Aboutus/how_work/Work.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import "./work.css"
+
+
+const work = () => {
+  return (
+    <div>
+    <h2 className='titleh'>How we work</h2>
+    <div className='work'>
+        <div className='step1'>
+        <p>
+        Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit.Omnis eius rerum 
+        repellendus,expedita numquam tenetur min
+        us aliquid!Qui obcaecati officia maiorescaecati 
+    
+        </p>
+        
+      </div>
+
+      <div className='step2'>
+        <p>
+        Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit.Omnis eius rerum 
+        repellendus,expedita numquam tenetur min
+        us aliquid!Qui obcaecati officia maiorescaecati 
+    
+        </p>
+        
+      </div>
+      <div className='step1'>
+        <p>
+        Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit.Omnis eius rerum 
+        repellendus,expedita numquam tenetur min
+        us aliquid!Qui obcaecati officia maiorescaecati 
+    
+        </p>
+        
+      </div>
+      <div className='step2'>
+        <p>
+        Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit.Omnis eius rerum 
+        repellendus,expedita numquam tenetur min
+        us aliquid!Qui obcaecati officia maiorescaecati 
+    
+        </p>
+        
+      </div>
+      <div className='step1'>
+        <p>
+        Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit.Omnis eius rerum 
+        repellendus,expedita numquam tenetur min
+        us aliquid!Qui obcaecati officia maiorescaecati 
+    
+        </p>
+        
+      </div>
+      </div>
+
+
+        </div>
+  )
+}
+
+export default work
---
+++ src/components/Aboutus/mission/mission.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import "./mission.css"
+
+function mission() {
+  return (
+    <div className='mainmission'>
+     <h2 className='titleM'>Our  mission</h2> 
+     <div>
+     <div className='Mgrid'>
+      
+
+<div>
+      <p>
+      Lorem Ipsualley of type and
+       scrambled it to make a type  
+      specimen book.Lorem Ipsum 
+      itting industryLorem Ipsum 
+      </p>       
+      
+      </div>
+      <div>
+      <img className='m1'
+      src="../Aboutimage/vector.png"  alt="" />
+</div>
+      <div>
+      <img className='m2'
+      src="../Aboutimage/vector1.png"  alt="" />
+</div>
+<div>
+      <p>
+      Lorem Ipsualley of type and
+       scrambled it to make a type  
+      specimen book.Lorem Ipsum 
+      itting industryLorem Ipsum 
+      </p>       
+      
+      </div>
+      
+      <div>
+      <p>
+      Lorem Ipsualley of type and
+       scrambled it to make a type  
+      specimen book.Lorem Ipsum 
+      itting industryLorem Ipsum 
+      </p>       
+      
+      </div>
+      <div>
+      <img className='m3'
+      src="../Aboutimage/vector 2.png"  alt="" />
+</div>
+      <div>
+      <img className='m4'
+      src="../Aboutimage/vector 3.png"  alt="" />
+</div>
+<div>
+      <p>
+      Lorem Ipsualley of type and
+       scrambled it to make a type  
+      specimen book.Lorem Ipsum 
+      itting industryLorem Ipsum 
+      </p>       
+      
+      </div>
+
+
+     </div>
+     </div>
+
+      </div>
+  )
+}
+
+export default mission
