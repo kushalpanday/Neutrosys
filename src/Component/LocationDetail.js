da4810f8ec68a8913f5f17a1626091d32a5004cd
Ranjananeutroline <Ranjana.neutroline@gmail.com>
2022-10-19 11:12:23 +0545

    first commit

--- src/Component/LocationDetail.js
+++ src/Component/LocationDetail.js
@@ -45,7 +45,7 @@ export default function LocationDetail() {
                         </Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                           
+                       <iframe style={{height:'450px', width:'100%'}} id="gmap_canvas" src="https://maps.google.com/maps?q=neutrosys.Inc&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
                         </Modal.Body>
                     </Modal>
                                     
