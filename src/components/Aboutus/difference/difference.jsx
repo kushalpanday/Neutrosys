b345464765ad68c4b06a6d24827a55dd3ab7325f
Pranish Shrestha <pranishshrestha181@gmail.com>
2022-10-20 06:18:59 +0545

    fix: responsive of what makes nuetroline different

--- src/components/Aboutus/difference/difference.jsx
+++ src/components/Aboutus/difference/difference.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import "./difference.css"
+import React from "react";
+import "./difference.css";
 function WhoWeAre() {
   return (
-   <div>
-    <h2 className='text1'>What makes Neutrosys different?</h2>
-    <div className="second">
-    <div className="none"></div>
-    <div className="image"><img src="../Aboutimage/what.PNG" alt="" /></div>
-    <div className="textBox">
-    <h2>Neutrosys</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis eius rerum repellendus,
-     expedita numquam tenetur minus aliquid! Qui obcaecati officia maiores placeat ducimus
-     expedita numquam tenetur minus aliquid! Qui obcaecati officia maiores placeat ducimus
-     expedita numquam tenetur minus aliquid! Qui obcaecati officia maiores placeat ducimus
-     atque voluptatum voluptate tenetur numquam tenetur minus aliquid! Qui obcaecati officia maiores placeat.</p>
-
-    </div>
-    <div className="nonee"></div>
-    </div>
-    
+    <div>
+      <h2 className="text1">What makes Neutrosys different?</h2>
+      <div className="second">
+        {/* <div className="none"></div> */}
+        <div className="image">
+          <img src="../Aboutimage/what.PNG" alt="" />
         </div>
-   
-  )
+        <div className="textBox">
+          <h2>Neutrosys</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis eius
+            rerum repellendus, expedita numquam tenetur minus aliquid! Qui
+            obcaecati officia maiores placeat ducimus expedita numquam tenetur
+            minus aliquid! Qui obcaecati officia maiores placeat ducimus
+            expedita numquam tenetur minus aliquid! Qui obcaecati officia
+            maiores placeat ducimus atque voluptatum voluptate tenetur numquam
+            tenetur minus aliquid! Qui obcaecati officia maiores placeat.
+          </p>
+        </div>
+        {/* <div className="nonee"></div> */}
+      </div>
+    </div>
+  );
 }
 
-export default WhoWeAre
+export default WhoWeAre;
